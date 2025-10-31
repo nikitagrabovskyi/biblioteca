b6013089a3c1cf6dfa3213a1b5ad2f783eaec730
nikitagrabovskyi <zmn22239@educastur.es>
2025-10-31 20:27:01 +0100

    aaa

--- biblioteca/biblioteca/src/main/java/com/example/biblioteca/controller/LibroController.java
+++ biblioteca/biblioteca/src/main/java/com/example/biblioteca/controller/LibroController.java
@@ -2,6 +2,7 @@ package com.example.biblioteca.controller;
 
 import com.example.biblioteca.entity.Libro;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 import com.example.biblioteca.service.LibroServicelmpl;
 
@@ -10,6 +11,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/api")
+@Service
 public class LibroController {
 
     @Autowired
